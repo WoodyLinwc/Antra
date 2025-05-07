00f8ba7e96dec246969156f7cc22479d2655228c
Woody Lin <wancheng.lin001@umb.edu>
2025-05-07 12:35:48 -0400

    ...

--- React/react-project/src/App.jsx
+++ React/react-project/src/App.jsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef, useEffect } from 'react'
+import TodoList from './TodoList'
+import { v4 as uuidv4 } from 'uuid';
+
+
+const KEY = 'todoApp.todos';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const todoRefName = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(KEY));
+    console.log("Loading todos:", storedTodos);
+    if(storedTodos){
+      setTodos(storedTodos);
+    }
+  }, [])
+
+
+  useEffect(() => {
+    console.log("Saving todos:", todos);
+    localStorage.setItem(KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function handleAddTodo(e){
+    const name = todoRefName.current.value;
+    if(name === '') return;
+    setTodos(prevTodo => {
+      return [...prevTodo, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoRefName.current.value = null;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <input ref={todoRefName} type="text" />
+     <button onClick={handleAddTodo}>Add Todo</button>
+     <button>Clear Complete</button>
+     <div>0 left to do</div>
+     <TodoList todos={todos}/>
     </>
+   
   )
 }
 
