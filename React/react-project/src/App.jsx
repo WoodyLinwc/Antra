90e8dc5715ee59f178c628079ebbe04413e9834a
Woody Lin <wancheng.lin001@umb.edu>
2025-05-07 12:39:52 -0400

    ...

--- React/react-project/src/App.jsx
+++ React/react-project/src/App.jsx
@@ -32,13 +32,20 @@ function App() {
     todoRefName.current.value = null;
   }
 
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find(todo => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
+
   return (
     <>
      <input ref={todoRefName} type="text" />
      <button onClick={handleAddTodo}>Add Todo</button>
      <button>Clear Complete</button>
      <div>0 left to do</div>
-     <TodoList todos={todos}/>
+     <TodoList todos={todos} toggleTodo={toggleTodo}/>
     </>
    
   )
