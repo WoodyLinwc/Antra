f4c015a79b85854116ed5d6ea837f8c763db70c3
Woody Lin <wancheng.lin001@umb.edu>
2025-05-08 14:17:16 -0400

    Update ClassDemo.jsx

--- React/class-function-component/src/ClassDemo.jsx
+++ React/class-function-component/src/ClassDemo.jsx
@@ -8,7 +8,8 @@ class ClassDemo extends React.Component{
 
         this.state = {
             counter: 0,
-            timer: 0
+            timer: 0,
+            intervalId: null
         }
     }
 
@@ -27,22 +28,67 @@ class ClassDemo extends React.Component{
     };
 
     render(){
+        console.log("Class component rendered");
         return (
             <div className="card">
             <h3>Class Component Demo</h3>
             <p>Counter: {this.state.counter}</p>
+            <p>Timer: {this.state.timer} seconds</p>
             <button onClick={this.handleAdd}>Add</button>
             <button onClick={this.clearCount}>Reset</button>
             </div>
         )
     }
 
+    // Runs once after initial render
     componentDidMount(){
+        if (this.state.intervalId) {
+            clearInterval(this.state.intervalId);
+        }
+        // console.log("Component mounted");
+        // 1. Setting up timers/intervals
+        // 2. API calls
+        // 3. Adding event listeners
+        const intervalId = setInterval(() => {
+            this.setState(prevState => ({
+                timer: prevState.timer + 1
+            }));
+        }, 1000);
+
+        // Save interval ID to state so we can clear it later
+        this.setState({ intervalId });
+    }
+
+    // Runs after every update to state or props
+    componentDidUpdate(prevProps, prevState){
+        // console.log("Component updated");
+        // 1. Responding to state/prop changes
+        // 2. Network requests based on prop changes
+        // 3. DOM manipulations after update
+
+        // Log when counter changes
+        if (prevState.counter !== this.state.counter) {
+            console.log(`Counter changed from ${prevState.counter} to ${this.state.counter}`);
+        }
+
+        // Do something when timer reaches certain value
+        if (prevState.timer !== this.state.timer && this.state.timer % 5 === 0 && this.state.timer > 0) {
+            console.log(`Timer reached ${this.state.timer} seconds!`);
+        }
 
     }
 
-    componentDidUpdate(){
-        
+    // Runs before component is removed from DOM
+    componentWillUnmount(){
+        console.log("Component will unmount");
+        // 1. Cleanup (remove event listeners, timers, subscriptions)
+        // 2. Cancel network requests
+        // 3. Clear intervals
+
+        clearInterval(this.state.intervalId);
+
+        console.log("Cleanup complete");
+
     }
 }
 
