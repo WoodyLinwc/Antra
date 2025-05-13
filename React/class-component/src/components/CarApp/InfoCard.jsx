fcd93a2c1b18cd4ae580f7847e05cd8d4a96a7e3
Woody Lin <wancheng.lin001@umb.edu>
2025-05-12 22:52:41 -0400

    Create InfoCard.jsx

---
+++ React/class-component/src/components/CarApp/InfoCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+// React.PureComponent: shallow comparison
+class InfoCard extends React.Component{
+    render() {
+
+        // const { car } = this.props;
+        // const { make, quantity, id } = car;
+        const {make, quantity, id} = this.props.car;
+        console.log(`${make} rendered`);
+
+        return(
+            <div style={{width: "100px", height: "100px", border: "1px black solid"}}>
+                <div>{make}</div>
+                <div>{quantity}</div>
+                {/* <div>{this.props.car.id}</div> */}
+                <button onClick={() => this.props.handleSell(id)}>Sell</button>
+            </div>
+        )
+
+    }
+}
+
+export default InfoCard;
