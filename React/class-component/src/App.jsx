0c9c5d6b20af25203aea802c2c100a4d2ef3b474
Woody Lin <wancheng.lin001@umb.edu>
2025-05-12 22:52:38 -0400

    Update App.jsx

--- React/class-component/src/App.jsx
+++ React/class-component/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+import CarApp from "./components/CarApp/CarApp"
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello</h1>
+      <CarApp/>
     </>
   )
 }
