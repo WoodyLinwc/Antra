97f4501896d6cf548c5eb31a8a38b0a33ad6f2e4
Woody Lin <wancheng.lin001@umb.edu>
2025-05-13 21:00:47 -0400

    Update App.jsx

--- React_Basics/src/App.jsx
+++ React_Basics/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import CarApp from './components/CarApp/CarApp';
+import Timer from './components/Timer/Timer';
+import Counter from './components/Counter/Counter';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello</h1>
       <CarApp/>
+      <hr />
+      <Timer/>
+      <hr />
+      <Counter/>
+      <hr />
     </>
   )
 }
