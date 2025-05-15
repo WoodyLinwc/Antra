16811d8187e92d60a6b18885bebc4f6f02aaf9e2
Woody Lin <wancheng.lin001@umb.edu>
2025-05-15 13:52:07 -0400

    Update App.jsx

--- React_Basics/src/App.jsx
+++ React_Basics/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import CarApp from './components/CarApp/CarApp';
 import Timer from './components/Timer/Timer';
 import Counter from './components/Counter/Counter';
+import Shopping from './components/Shopping/Shopping';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <hr />
       <Counter/>
       <hr />
+      <Shopping/>
     </>
   )
 }
