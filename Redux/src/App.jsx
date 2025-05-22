f512d21d088a6ce66f2b3acd5880ca579191c189
Woody Lin <wancheng.lin001@umb.edu>
2025-05-22 00:21:35 -0400

    renaming js to jsx

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { StoreProvider, createStore } from "./redux/store";
+import { counterReducer } from "./redux/counterReducer";
 import { Counter, CounterDisplay } from "./components/Counter";
+
+const store = createStore(counterReducer, { count: 0 });
 
 function App() {
     return (
         <>
-            <StoreProvider>
+            <StoreProvider store={store}>
                 <div>
                     <h1>Custom Redux Implementation</h1>
                     <Counter />
