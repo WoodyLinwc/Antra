77cf464d14fc8745220a8b452d73700a710a1032
Woody Lin <wancheng.lin001@umb.edu>
2025-05-29 23:30:37 -0400

    Update users.js

--- ExpressJS/data/users.js
+++ ExpressJS/data/users.js
@@ -5,16 +5,14 @@ let users = [
         id: uuidv4(),
         username: "Woody",
         // This is a hashed version of "admin123"
-        password:
-            "$2b$10$X5mOE2xl9jJ6ZVUYg5w3/.zvjV.a7JI3NpLGL1H1n7gOKvEhAUJFy",
+        password: "admin123",
         role: "admin",
     },
     {
         id: uuidv4(),
         username: "user",
         // This is a hashed version of "user123"
-        password:
-            "$2b$10$RqH9eXEOvYy3pw7XV/a9WuDsuNhZLAKBYz.4MxKNnRmLoGFhK3kEC",
+        password: "user123",
         role: "user",
     },
 ];
