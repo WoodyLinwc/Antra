04b3bf427664ce62828caf943206d0bcbc66eedd
Woody Lin <wancheng.lin001@umb.edu>
2025-05-31 23:43:03 -0400

    simple todo list backend

--- todolist_fullstack/backend/app.js
+++ todolist_fullstack/backend/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const todosRoute = require("./routes/todos");
+
+app.use(express.json());
+app.use("/api/todos", todosRoute);
+
 app.get("/", (req, res) => {
     res.send("Backend for the todo list");
 });
---
+++ todolist_fullstack/backend/database/data.js
@@ -0,0 +1,18 @@
+let todos = [
+    {
+        id: "1",
+        title: "Working out",
+        description: "twice a week",
+        completed: false,
+    },
+    {
+        id: "2",
+        title: "Shopping",
+        description: "once a week",
+        completed: true,
+    },
+];
+
+module.exports = {
+    todos,
+};
