925f328b1953c4482336707795bcb2d629cb5511
Woody Lin <wancheng.lin001@umb.edu>
2025-06-04 13:22:03 -0400

    Update app.js

--- todolist_fullstack/backend/app.js
+++ todolist_fullstack/backend/app.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 3000;
 
 const todosRoute = require("./routes/todos");
+const authRoute = require("./routes/auth");
 
 app.use(express.json());
 app.use("/api/todos", todosRoute);
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
     res.send("Backend for the todo list");
